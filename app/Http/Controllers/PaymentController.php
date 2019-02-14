6ddd6e593df76f780cfd09119e16c6488749ea8f
vagrant <vagrant@homestead>
2019-02-14 18:25:42 +0000

    payments

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -137,7 +137,13 @@ class PaymentController extends Controller
     
             if ($data->type == 'charge.paid'){
                 
+                $order_id   =  $data->data->object->order_id;
+                $status     =  $data->data->object->status;
+                $payment    =  Payment::where('order_id',$order_id)->first();
 
+                $payment->status = $status;
+                $payment->save();
+                
                 // Enviamos un correo a la compañía con la información de todas las reservaciones
                 // Mail::send('reyapp.mails.confirmation_com', ['room_name'=>$room_name,'reservations'=>$reservations,'company'=>$company,'instructions'=>$instructions], function ($message)use($company_email,$room_name){
 
