a4c1f3a61848199f24c9e6c4bd65b7388ecc7d1e
Sergio García <cheko@Sergios-MacBook-Air.local>
2019-02-15 14:22:34 -0600

    paypal test

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -152,12 +152,42 @@ class PaymentController extends Controller
 
 	public function confirmation_paypal(Request $request){
 			
-			
-			$body = @file_get_contents('php://input');
-			$data = json_decode($body);
+	   
+	    // read the post from PayPal system and add 'cmd'  
+	    $req = 'cmd=_notify-validate';  
+	    foreach ($_POST as $key => $value) {  
+	    	$value = urlencode(stripslashes($value));  
+	    	$req .= "&$key=$value";
 
-			http_response_code(200); // Return 200 OK 
-			Log::info(print_r($data, true));
+	    }  
+	    // post back to PayPal system to validate  
+	    $header = "POST /cgi-bin/webscr HTTP/1.0\r\n";  
+	    $header .= "Content-Type: application/x-www-form-urlencoded\r\n";  
+	    $header .= "Content-Length: " . strlen($req) . "\r\n\r\n";  
+
+	    $fp = fsockopen ('www.sandbox.paypal.com', 80, $errno, $errstr, 30); 
+	     
+	    if (!$fp) {  
+	    // HTTP ERROR  
+	    } else {  
+		    fputs ($fp, $header . $req);  
+		    while (!feof($fp)) {  
+		    	$res = fgets ($fp, 1024);  
+			    if (strcmp ($res, "VERIFIED") == 0) {  
+
+				    // PAYMENT VALIDATED & VERIFIED!  
+				    $email = $_POST['payer_email']; 
+				    $gross = $_POST['mc_gross'];  
+				    $name = $_POST['address_name']; 
+				    $pupilname = $_POST['item_name']; 
+				    $pupilno = $_POST['item_number']; 
+
+				     
+
+			    }  
+		    }  
+	    	fclose ($fp);  
+	    } 
 			// $payment                    = new Payment;
 			// $payment->order_id          = $_POST['invoice'];
 			// $payment->amount            = '32';
