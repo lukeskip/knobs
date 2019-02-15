2c4ef1e4f64ffad8ffcce8efbad0bef269afe6cc
Sergio García <cheko@Sergios-MacBook-Air.local>
2019-02-15 12:46:40 -0600

    paypal test

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -151,30 +151,78 @@ class PaymentController extends Controller
 
 	public function confirmation_paypal(Request $request){
 
-			// Response from Paypal
-			// read the post from PayPal system and add 'cmd'
-			// $req = 'cmd=_notify-validate';
-			// foreach ($_POST as $key => $value) {
-			// 	$value = urlencode(stripslashes($value));
-			// 	$value = preg_replace('/(.*[^%^0^D])(%0A)(.*)/i','${1}%0D%0A${3}',$value);// IPN fix
-			// 	$req .= "&$key=$value";
-			// }
+			raw_post_data = file_get_contents('php://input');
+$raw_post_array = explode('&', $raw_post_data);
+$myPost = array();
+foreach ($raw_post_array as $keyval) {
+  $keyval = explode ('=', $keyval);
+  if (count($keyval) == 2)
+    $myPost[$keyval[0]] = urldecode($keyval[1]);
+}
+// read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
+$req = 'cmd=_notify-validate';
+if (function_exists('get_magic_quotes_gpc')) {
+  $get_magic_quotes_exists = true;
+}
+foreach ($myPost as $key => $value) {
+  if ($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
+    $value = urlencode(stripslashes($value));
+  } else {
+    $value = urlencode($value);
+  }
+  $req .= "&$key=$value";
+}
 
-			// // assign posted variables to local variables
-			// $data['item_name']          = $_POST['item_name'];
-			// $data['item_number']        = $_POST['item_number'];
-			// $data['payment_status']     = $_POST['payment_status'];
-			// $data['payment_amount']     = $_POST['mc_gross'];
-			// $data['payment_currency']   = $_POST['mc_currency'];
-			// $data['txn_id']             = $_POST['txn_id'];
-			// $data['receiver_email']     = $_POST['receiver_email'];
-			// $data['payer_email']        = $_POST['payer_email'];
-			// $data['custom']             = $_POST['custom'];
-			// $data['invoice']            = $_POST['invoice'];
-			// $data['paypallog']          = $req;
+// inspect IPN validation result and act accordingly
+if (strcmp ($res, "VERIFIED") == 0) {
+  // The IPN is verified, process it:
+  // check whether the payment_status is Completed
+  // check that txn_id has not been previously processed
+  // check that receiver_email is your Primary PayPal email
+  // check that payment_amount/payment_currency are correct
+  // process the notification
+  // assign posted variables to local variables
+  $item_name = $_POST['item_name'];
+  $item_number = $_POST['item_number'];
+  $payment_status = $_POST['payment_status'];
+  $payment_amount = $_POST['mc_gross'];
+  $payment_currency = $_POST['mc_currency'];
+  $txn_id = $_POST['txn_id'];
+  $receiver_email = $_POST['receiver_email'];
+  $payer_email = $_POST['payer_email'];
+  // IPN message values depend upon the type of notification sent.
+  // To loop through the &_POST array and print the NV pairs to the screen:
+  foreach($_POST as $key => $value) {
+    echo $key . " = " . $value . "<br>";
+  }
+} else if (strcmp ($res, "INVALID") == 0) {
+  // IPN invalid, log for manual investigation
+  echo "The response from IPN was: <b>" .$res ."</b>";
+}
+
+// Step 2: POST IPN data back to PayPal to validate
+$ch = curl_init('https://ipnpb.paypal.com/cgi-bin/webscr');
+curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
+// In wamp-like environments that do not come bundled with root authority certificates,
+// please download 'cacert.pem' from "https://curl.haxx.se/docs/caextract.html" and set
+// the directory path of the certificate as shown below:
+// curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
+if ( !($res = curl_exec($ch)) ) {
+  // error_log("Got " . curl_error($ch) . " when processing IPN data");
+  curl_close($ch);
+  exit;
+}
+
 
 			$payment                    = new Payment;
-			$payment->order_id          = $_POST['invoice'];
+			$payment->order_id          = $item_number;
 			$payment->amount            = '32';
 			$payment->total             = '23';
 			$payment->method            = '234234';
@@ -186,7 +234,7 @@ class PaymentController extends Controller
 			$payment->user_id           = 1;
 			$payment->save();
 
-			
+curl_close($ch);			
 
    
 	 
