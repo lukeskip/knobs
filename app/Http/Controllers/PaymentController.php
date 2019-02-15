ac4b54eca4f3ecf72e7f753688c31032dd51a31d
Sergio García <cheko@Sergios-MacBook-Air.local>
2019-02-15 12:37:42 -0600

    paypal test

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -12,255 +12,282 @@ use Jenssegers\Date\Date;
 
 class PaymentController extends Controller
 {
-    public function __construct()
-    {
-            \Conekta\Conekta::setApiKey(env('CONEKTA_SECRET_KEY'));
-            \Conekta\Conekta::setApiVersion("2.0.0");
-    }
+	public function __construct()
+	{
+			\Conekta\Conekta::setApiKey(env('CONEKTA_SECRET_KEY'));
+			\Conekta\Conekta::setApiVersion("2.0.0");
+	}
 
-    //Creando un pago mediante Oxxo.
-    public function CreatePayOxxo(Request $request)
-    {         
-        $options     = Option::all(); 
-        $song_id     = $request->song_id;
-        $price_knob  = 200;
-        $now         = Date::now()->add('12 day');
-        $expire      = strtotime($now);
-        $user_id     = Auth::user()->id;   
+	//Creando un pago mediante Oxxo.
+	public function CreatePayOxxo(Request $request)
+	{         
+		$options     = Option::all(); 
+		$song_id     = $request->song_id;
+		$price_knob  = 200;
+		$now         = Date::now()->add('12 day');
+		$expire      = strtotime($now);
+		$user_id     = Auth::user()->id;   
 
-        $valid_order =
-            array(
-                'line_items'=> array(
-                    array(
-                        'name'        => "Cargo por knob",
-                        'description' => 'Cargo por crítica de una canción',
-                        'unit_price'  => $price_knob * 100,//El costo se pasa en centavos
-                        'quantity'    => 1,
-                        'metadata' => array(
-                            'type' => 'natural'
-                    )
-                ),  
-            ),
+		$valid_order =
+			array(
+				'line_items'=> array(
+					array(
+						'name'        => "Cargo por knob",
+						'description' => 'Cargo por crítica de una canción',
+						'unit_price'  => $price_knob * 100,//El costo se pasa en centavos
+						'quantity'    => 1,
+						'metadata' => array(
+							'type' => 'natural'
+					)
+				),  
+			),
 
-            'currency'      => 'MXN',
-                // 'metadata'    => array('test' => 'extra info'),
-            'charges'     => array(
-                array(
-                    'payment_method' => array(
-                            'type'       => 'oxxo_cash',
-                            'expires_at' => $expire
-                    )
-                    //'amount' => $precio
-                )
-            ),
-            'currency'      => 'MXN',
-            'customer_info' => array(
-                    'name'  => "Sergio Garcia",
-                    'phone' => "5535555637",
-                    // 'email' => $request->input('email')
-                    'email' => 'contacto@chekogarcia.com.mx'
-            )
-        );
-
-
-        try {
-            $order = \Conekta\Order::create($valid_order);
-            $pI = $order['id'];
-            $pM = $order->charges[0]->payment_method->type;
-            $pR = $order->charges[0]->payment_method->reference;
-            $pS = $order['payment_status'];
-
-            $rsp = array("id"=>$pI,"method"=>$pM,"reference"=>$pR,"status"=>$pS);
-                
-
-            $pI = $order['id'];
-            $pM = $order->charges[0]->payment_method->type;
-            $pS = $order['payment_status'];
-            $pT = $order->amount /100; //Total de la transacción
-
-            // Declaramos las variables que serán ocupadas en los costos
-            $pQ    = 0;
-            $pUP   = 0;
-            $pC    = 0;
-            $pQP   = 0;
-            $pUPP  = 0;
-            
-            // asignamos la cantidad de horas y comision
-            foreach ($order->line_items as $line) {
-                
-                $line_type = $line['metadata']['type'];  
-                $pQ  = $line->quantity;
-                $pUP = $line->unit_price / 100;
-
-                
-                
-            }
-
-            $pA = (($pUP * $pQ)+($pUPP * $pQP));//cargo sin contar la comision
-            
-            $pE = $order->charges[0]->payment_method->expires_at;
-            // $rsp = array("id"=>$pI,"method"=>$pM,"reference"=>$pR,"status"=>$pS,'price'=>$price);
-
-            $payment                    = new Payment;
-            $payment->order_id          = $pI;
-            $payment->amount            = $pA;
-            $payment->total             = $pT;
-            $payment->method            = $pM;
-            $payment->status            = $pS;
-            $payment->reference         = $pR;
-            $payment->expires_at        = $pE;
-            $payment->status            = 'pending';
-            $payment->song_id           = $song_id;
-            $payment->user_id           = $user_id;
-            $payment->save();
+			'currency'      => 'MXN',
+				// 'metadata'    => array('test' => 'extra info'),
+			'charges'     => array(
+				array(
+					'payment_method' => array(
+							'type'       => 'oxxo_cash',
+							'expires_at' => $expire
+					)
+					//'amount' => $precio
+				)
+			),
+			'currency'      => 'MXN',
+			'customer_info' => array(
+					'name'  => "Sergio Garcia",
+					'phone' => "5535555637",
+					// 'email' => $request->input('email')
+					'email' => 'contacto@chekogarcia.com.mx'
+			)
+		);
 
 
-            
-            return response()->json(['success' => true,'message'=>$pS,'code'=>$payment->order_id]);
+		try {
+			$order = \Conekta\Order::create($valid_order);
+			$pI = $order['id'];
+			$pM = $order->charges[0]->payment_method->type;
+			$pR = $order->charges[0]->payment_method->reference;
+			$pS = $order['payment_status'];
 
-        } catch (\Conekta\ProcessingError $e){ 
-            return $this->Response(0,$e);
-        } catch (\Conekta\ParameterValidationError $e){
-                return $this->Response(0,$e);
-        } 
-        catch (\Conekta\Handler $e){
-                return $this->Response(0,$e);
-        }
+			$rsp = array("id"=>$pI,"method"=>$pM,"reference"=>$pR,"status"=>$pS);
+				
 
-    }
+			$pI = $order['id'];
+			$pM = $order->charges[0]->payment_method->type;
+			$pS = $order['payment_status'];
+			$pT = $order->amount /100; //Total de la transacción
 
-    public function confirmation(){
+			// Declaramos las variables que serán ocupadas en los costos
+			$pQ    = 0;
+			$pUP   = 0;
+			$pC    = 0;
+			$pQP   = 0;
+			$pUPP  = 0;
+			
+			// asignamos la cantidad de horas y comision
+			foreach ($order->line_items as $line) {
+				
+				$line_type = $line['metadata']['type'];  
+				$pQ  = $line->quantity;
+				$pUP = $line->unit_price / 100;
 
-            $body = @file_get_contents('php://input');
-            $data = json_decode($body);
-            http_response_code(200); // Return 200 OK 
-    
-            if ($data->type == 'charge.paid'){
-                
-                $order_id   =  $data->data->object->order_id;
-                $status     =  $data->data->object->status;
-                $payment    =  Payment::where('order_id',$order_id)->first();
+				
+				
+			}
 
-                $payment->status = $status;
-                $payment->save();
+			$pA = (($pUP * $pQ)+($pUPP * $pQP));//cargo sin contar la comision
+			
+			$pE = $order->charges[0]->payment_method->expires_at;
+			// $rsp = array("id"=>$pI,"method"=>$pM,"reference"=>$pR,"status"=>$pS,'price'=>$price);
 
-
-            }       
-            
-    }
-
-    public function confirmation_paypal(Request $request){
-
-            $payment                    = new Payment;
-            $payment->order_id          = '111';
-            $payment->amount            = '32';
-            $payment->total             = '23';
-            $payment->method            = '234234';
-            $payment->status            = '234234';
-            $payment->reference         = '234234';
-            $payment->expires_at        = '234234';
-            $payment->status            = 'pending';
-            $payment->song_id           = 1;
-            $payment->user_id           = 1;
-            $payment->save();
-    
-            // if ($data->type == 'charge.paid'){
-                
-            //     $order_id   =  $data->data->object->order_id;
-            //     $status     =  $data->data->object->status;
-            //     $payment    =  Payment::where('order_id',$order_id)->first();
-
-            //     $payment->status = $status;
-            //     $payment->save();
+			$payment                    = new Payment;
+			$payment->order_id          = $pI;
+			$payment->amount            = $pA;
+			$payment->total             = $pT;
+			$payment->method            = $pM;
+			$payment->status            = $pS;
+			$payment->reference         = $pR;
+			$payment->expires_at        = $pE;
+			$payment->status            = 'pending';
+			$payment->song_id           = $song_id;
+			$payment->user_id           = $user_id;
+			$payment->save();
 
 
-            // }       
-            
-    }
+			
+			return response()->json(['success' => true,'message'=>$pS,'code'=>$payment->order_id]);
 
-        
-        //Manejo de respuestas
-        private function Response($success,$result)
-        {
-                if($success==0){ $result=$result->getMessage(); } 
-                $out = array("type"=>$success,"data"=>$result);
-                return view('sweet.testResponse')->with('response',$out); 
-        }
+		} catch (\Conekta\ProcessingError $e){ 
+			return $this->Response(0,$e);
+		} catch (\Conekta\ParameterValidationError $e){
+				return $this->Response(0,$e);
+		} 
+		catch (\Conekta\Handler $e){
+				return $this->Response(0,$e);
+		}
+
+	}
+
+	public function confirmation(){
+
+			$body = @file_get_contents('php://input');
+			$data = json_decode($body);
+			http_response_code(200); // Return 200 OK 
+	
+			if ($data->type == 'charge.paid'){
+				
+				$order_id   =  $data->data->object->order_id;
+				$status     =  $data->data->object->status;
+				$payment    =  Payment::where('order_id',$order_id)->first();
+
+				$payment->status = $status;
+				$payment->save();
 
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+			}       
+			
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(Song $song)
-    {
-        return view('sweet.payment_create')->with('song',$song);
-    }
+	public function confirmation_paypal(Request $request){
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+			// Response from Paypal
+			// read the post from PayPal system and add 'cmd'
+			// $req = 'cmd=_notify-validate';
+			// foreach ($_POST as $key => $value) {
+			// 	$value = urlencode(stripslashes($value));
+			// 	$value = preg_replace('/(.*[^%^0^D])(%0A)(.*)/i','${1}%0D%0A${3}',$value);// IPN fix
+			// 	$req .= "&$key=$value";
+			// }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Payment  $payment
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Payment $payment)
-    {
-        //
-    }
+			// // assign posted variables to local variables
+			// $data['item_name']          = $_POST['item_name'];
+			// $data['item_number']        = $_POST['item_number'];
+			// $data['payment_status']     = $_POST['payment_status'];
+			// $data['payment_amount']     = $_POST['mc_gross'];
+			// $data['payment_currency']   = $_POST['mc_currency'];
+			// $data['txn_id']             = $_POST['txn_id'];
+			// $data['receiver_email']     = $_POST['receiver_email'];
+			// $data['payer_email']        = $_POST['payer_email'];
+			// $data['custom']             = $_POST['custom'];
+			// $data['invoice']            = $_POST['invoice'];
+			// $data['paypallog']          = $req;
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Payment  $payment
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Payment $payment)
-    {
-        //
-    }
+			$payment                    = new Payment;
+			$payment->order_id          = $_POST['invoice'];
+			$payment->amount            = '32';
+			$payment->total             = '23';
+			$payment->method            = '234234';
+			$payment->status            = '234234';
+			$payment->reference         = '234234';
+			$payment->expires_at        = '234234';
+			$payment->status            = 'pending';
+			$payment->song_id           = 1;
+			$payment->user_id           = 1;
+			$payment->save();
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Payment  $payment
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Payment $payment)
-    {
-        //
-    }
+			
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Payment  $payment
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Payment $payment)
-    {
-        //
-    }
+   
+	 
+	
+			// if ($data->type == 'charge.paid'){
+				
+			//     $order_id   =  $data->data->object->order_id;
+			//     $status     =  $data->data->object->status;
+			//     $payment    =  Payment::where('order_id',$order_id)->first();
+
+			//     $payment->status = $status;
+			//     $payment->save();
+
+
+			// }       
+			
+	}
+
+		
+		//Manejo de respuestas
+		private function Response($success,$result)
+		{
+				if($success==0){ $result=$result->getMessage(); } 
+				$out = array("type"=>$success,"data"=>$result);
+				return view('sweet.testResponse')->with('response',$out); 
+		}
+
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create(Song $song)
+	{
+		return view('sweet.payment_create')->with('song',$song);
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Payment  $payment
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Payment $payment)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Payment  $payment
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Payment $payment)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Payment  $payment
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Payment $payment)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Payment  $payment
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Payment $payment)
+	{
+		//
+	}
 }
--- resources/views/sweet/payment_create.blade.php
+++ resources/views/sweet/payment_create.blade.php
@@ -58,60 +58,32 @@
 						
 					</div>
 					<div class="col-md-6">
-						<!-- <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
-    <input type="hidden" name="cmd" value="_xclick">
-    <input type="hidden" name="business" value="XXX">
-    <input type="hidden" name="lc" value="CA">
-    <input type="hidden" name="item_name" value="Tangled Roots">
-    <input type="hidden" name="button_subtype" value="services">
-    <input type="hidden" name="no_note" value="0">
-    <input type="hidden" name="cn" value="Add special instructions to the seller">
-    <input type="hidden" name="no_shipping" value="2">
-    <input name="amount" value="16.99">
-    <input type="hidden" name="currency_code" value="CAD">
-    <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted">
-    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form> -->
-
-		<form action='https://www.sandbox.paypal.com/cgi-bin/webscr' method='post'>
-
-
-		<input type='hidden' name='business' value='contacto-facilitator@chekogarcia.com.mx'>
-
-
-		<input type='hidden' name='cmd' value='_xclick'>
-
-
-		<input type='hidden' name='item_name' value='Hot Sauce-12 oz. Bottle'>
-		<input type='hidden' name='amount' value='5.95'>
-		<input type='hidden' name='currency_code' value='MXN'>
-		<input name="notify_url" value="https://knobs.reydecibel.com.mx" type="hidden">
-
-		<input type="hidden" name="return" value="https://knobs.reydecibel.com.mx">
-<input type="hidden" name="notify_url" value="https://knobs.reydecibel.com.mx/confirmed_paypal">
-
-
-
-		<input type='image' name='submit' border='0' src='https://www.paypal.com/en_US/i/btn/btn_buynow_LG.gif' alt='PayPal – The safer, easier way to pay online'>
-		<img alt='' border='0' width='1' height='1' src='https://www.paypal.com/en_US/i/scr/pixel.gif' >
-
-		</form>
-
-
-
-Read more: http://designertuts.com/adding-an-advanced-paypal-buy-now-button.php#ixzz5fciVS8uH
-
-
-
-
 						
+
+						<form action='https://www.sandbox.paypal.com/cgi-bin/webscr' method='post'>
+
+
+							<input type='hidden' name='business' value='contacto-facilitator@chekogarcia.com.mx'>
+
+
+							<input type='hidden' name='cmd' value='_xclick'>
+
+
+							<input type='hidden' name='item_name' value='Hot Sauce-12 oz. Bottle'>
+							<input type='hidden' name='amount' value='5.95'>
+							<input type='hidden' name='currency_code' value='MXN'>
+							<input name="notify_url" value="https://knobs.reydecibel.com.mx" type="hidden">
+
+							<input type="hidden" name="return" value="https://knobs.reydecibel.com.mx">
+							<input type="hidden" name="notify_url" value="https://knobs.reydecibel.com.mx/confirmed_paypal">
+
+							<button class="btn btn-success btn-lg btn-block paypal">Pagar con Paypal</button>
+
+						</form>
+			
 					</div>
 				</div>
-			<div class="col-md-6">
-
-				
-				</div>
+			
 						
 					
 				
