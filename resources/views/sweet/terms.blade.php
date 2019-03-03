b5b5893021cd6bfd3276c9fb6e84c1a9eeb1b59c
Sergio García <cheko@Sergios-MacBook-Air.local>
2019-03-03 11:25:34 -0600

    incomplete terms

---
+++ resources/views/sweet/terms.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.main',['body_class' => 'dashboard'])
+
+@section('content')
+
+
+<div class="containe">
+	<div class="row">
+		<div class="col-md-12">
+			<h1>Términos y Condiciones</h1>
+			Knobs es una plataforma de intercambio de opiniones sobre música nueva. La plataforma el intercambio positivo de experiencias. Para mantener un ambiente de transparencia y cordialidad cada uno de los roles de usuario se comprometen:
+
+			<h2>Los administradores</h2>
+			<ul>
+				<li>A intervenir en caso de que músico y críticos tengan algún desacuerdo</li>
+				<li>A mantener en privacidad tanto los datos personales como el material artístico cargado a esta plataforma.</li>
+			</ul>
+
+			<h2>Los críticos</h2>
+			<ul>
+				<li>A dar su opinión acerca del material artístico de los músicos con profesionalismo y con la intención absoluta de coadyuvar al mejoramiento del mismos.</li>
+				<li>A proteger los derechos de autor de los materiales artísticos en esta plataforma, por lo que de ninguna manera podrán hacer uso de éstos sino es por permiso explícito de su autor.</li>
+				<li>
+					A 
+				</li>
+
+			</ul>
+
+			<h2>Los músicos</h2>
+			
+		</div>
+	</div>
+</div>
+
+
+
+@endsection
