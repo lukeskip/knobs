7e95afedb87637077fb8c2dd30d31f0d80b2f11b
Sergio García <cheko@Sergios-MacBook-Air.local>
2019-03-03 18:10:41 -0600

    terms

--- resources/views/sweet/notice_privacy.blade.php
+++ resources/views/sweet/notice_privacy.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-<div class="containe">
+<div class="container">
 	<div class="row">
 		<div class="col-md-12">
 			<h1>Aviso de Privacidad</h1>
--- resources/views/sweet/terms.blade.php
+++ resources/views/sweet/terms.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-<div class="containe">
+<div class="container">
 	<div class="row">
 		<div class="col-md-12">
 			<h1>Términos y Condiciones</h1>
