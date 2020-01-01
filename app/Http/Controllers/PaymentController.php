08f865dcde88ecac72de5b9abc751e597d5eba88
Cheko Garcia <contacto@chekogarcia.com.mx>
2020-01-01 14:27:57 -0600

    test 2

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -212,7 +212,7 @@ class PaymentController extends Controller
 		$song_id = $item_number[0];
 		$user_id = $item_number[1];
 		
-		$coupon = Coupon::find($custom);
+		$coupon = Coupon::find($_POST['custom']);
 		$coupon->redeemed = $coupon->redeemed +1;
 		$coupon->save();
 
